b82f6508b97ffc11b823270424b218df82b20875
Rodrigo Ramos Oliveira <rodrigojsg9@gmail.com>
2023-01-03 16:56:17 -0300

    meu novo exercício

---
+++ exercício_indice_lista.py
@@ -0,0 +1,12 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+indice = 0
+for nome in lista:
+    print(f'{nome} -> Indice: {indice}')
+    indice += 1
